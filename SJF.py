3b2ddb6c547dab037f62e69517e4c3827046b231
Aparnak12 <51270673+Aparnak12@users.noreply.github.com>
2020-08-23 00:08:16 +0530

    Update SJF.py
    sorting part is glitchy. works for some cases and does not for some others. else part for NZAT is still incomplete.

--- SJF.py
+++ SJF.py
@@ -1,7 +1,8 @@
 import operator
+from prettytable import PrettyTable
 
 def hasArrived(currentTime,arrivalTime) :
-    if(arrivalTime < currentTime) :
+    if(arrivalTime <= currentTime) :
         return True
     else :
         return False
@@ -11,12 +12,12 @@ def hasArrived(currentTime,arrivalTime) :
 n = int(input("Enter no. of processes : "))
 P = [] #list of tuples
 for i in range(0,n) :
-    AT = int(input("Enter arrival time of P"+str(i+1)+" : "))
-    BT = int(input("Enter burst time of P"+str(i+1)+" : "))
+    print("\nP"+str(i+1)+" :")
+    AT = int(input("Arrival time : "))
+    BT = int(input("Burst time : "))
     P.append((("P"+str(i+1)),AT,BT))
-    
-print(P)
 
+#split processes into zero and non-zero AT categories
 ZAT = []
 NZAT = []
 for process in P :
@@ -25,11 +26,9 @@ for process in P :
     else :
         NZAT.append(process)
 ZAT.sort(key = operator.itemgetter(2)) #sorting zero AT processes by their BT
-NZAT.sort(key = operator.itemgetter(2,1))
+NZAT.sort(key = operator.itemgetter(2,1)) #sorting non-zero AT by BT and AT respectively
 
-print(ZAT)
-print(NZAT)
-
+#calculation of gantt chart
 GC = []
 startTime = 0
 for ele in ZAT :
@@ -40,16 +39,42 @@ for ele in NZAT :
     if(hasArrived(startTime,ele[1])) :
         GC.append((ele[0],startTime,startTime+ele[2]))
         startTime = startTime + ele[2]
-    else :
-        
+    #else :
 
-zip_lists = zip(P,GC)
+#calculation of waiting and turnaround times in order of GC        
+zip_lists = zip(GC,P)
+newP = [x for _, x in sorted(zip_lists)] #original P array remains the same, newP is P sorted in order of GC
+zip_lists = zip(GC,newP)
+print(P)
+print(GC)
+print(newP)
 WT = []
 TT = []
 for (i,j) in zip_lists :
-    WT.append((i[0],j[1]-i[1]))
-    TT.append((i[0],j[1]-i[1]+i[2]))
-        
-print(GC)
-print("waiting times :",WT)
-print("turnaround times :",TT)
+    print("WT :",i[1]-j[1])
+    WT.append(i[1]-j[1])
+    print("TT :",i[1]-j[1]+j[2])
+    TT.append(i[1]-j[1]+j[2])
+
+#output
+
+print("\nImplementation of SJF Scheduling :")
+
+#GC
+print("\nGantt Chart :")
+for ele in GC :
+    print(ele[0],":",ele[1],"to",ele[2]) 
+    
+#WT and TT
+table = PrettyTable()
+table.add_column("Process",[ele[0] for ele in GC])
+table.add_column("Waiting Time",WT)
+table.add_column("Turnaround Time",TT)
+print("\n")
+print(table)
+
+#avg WT and avg TT       
+avg_wt = sum(WT)/n
+avg_tt = sum(TT)/n
+print("\nAverage Waiting Time =",avg_wt)
+print("\nAverage Turnaround Time =",avg_tt)
